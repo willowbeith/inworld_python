acde8b438ef653ab9098f60bcc7d1e63f1a13fbf
atorsvn <122282538+atorsvn@users.noreply.github.com>
2023-04-30 03:09:21 -0400

    Update __init__.py

--- inworld_python/__init__.py
+++ inworld_python/__init__.py
@@ -1,6 +1,1 @@
-from vidstream.streaming import StreamingServer
-from vidstream.streaming import CameraClient
-from vidstream.streaming import VideoClient
-from vidstream.streaming import ScreenShareClient
-from vidstream.audio import AudioSender
-from vidstream.audio import AudioReceiver
+from inworld_python import InWorldChat
