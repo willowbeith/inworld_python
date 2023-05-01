21096833520e666e115c1fc22ae6fbae5bc0b84c
atorsvn <122282538+atorsvn@users.noreply.github.com>
2023-05-01 05:33:59 -0400

    Update discord_bot.py

--- examples/discord_bot.py
+++ examples/discord_bot.py
@@ -2,26 +2,44 @@ import discord
 from discord.ext import commands
 from inworld_python import inworld_chat
 
+# Create a new Intents object with default settings
+# These settings include tracking of messages, reactions, etc.
 intents = discord.Intents.default()
+# Enable tracking of message content
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = "!", intents=intents)
+# Initialize the Bot object with the "!" command prefix
+# and the specified intents
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-chat_app = inworld_chat.InWorldChat('inworld_key', 
-                       'inworld_secret', 
-                       'inworld_scene'
-                      )
+# Create an InWorldChat object with the specified key, secret, and scene
+chat_app = inworld_chat.InWorldChat('inworld_key', 'inworld_secret', 'inworld_scene')
 
+# Set up the InWorldChat object
 chat_app.setup()
 
-#rename the command your bot name
 @bot.command()
 async def iw(ctx, *, query):
+    """
+    This function defines the 'iw' command for the bot.
+    When the 'iw' command is used, the bot sends a chat message using the InWorldChat object.
+    The message content is then sent as a reply in Discord.
+
+    :param ctx: context object provided by discord.py, contains message details
+    :param query: message content to send as chat
+    """
+    # Send a chat message using the InWorldChat object
+    # The message content, author name, channel ID, and author ID are provided as arguments
     out = chat_app.chat(query, str(ctx.author), str(ctx.channel.id), str(ctx.author.id))
+    # Reply in Discord with the chat output
     await ctx.reply(out)
 
 @bot.event
 async def on_ready():
+    """
+    This function is run when the bot has connected to Discord and is ready.
+    """
     print('Bot is ready')
 
+# Run the bot with the specified token
 bot.run('your-bot-token')
